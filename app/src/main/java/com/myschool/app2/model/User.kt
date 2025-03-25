d7a91b0ca8024c5d8bd855b44df68aaa459a7751
WindForest <munkored@yandex.ru>
2025-03-25 08:52:56 +0700

    viewBinding + User's model

---
+++ app/src/main/java/com/myschool/app2/model/User.kt
@@ -0,0 +1,10 @@
+package com.myschool.app2.model
+
+data class User(
+    val username: String,
+    val sex: String,
+    val address: String,
+    val name: String,
+    val email: String,
+    val birthday: String,
+)
